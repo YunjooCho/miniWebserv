b2c658557760cab2d288db50d4117187b0077af4
Yunjoo Cho <yunjcho@c1r7s6.42seoul.kr>
2023-11-01 20:06:53 +0900

    Make Server socket

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cerrno>
+#include <cstring>
+
+int	main(int ac, char **av)
+{
+	typedef struct sockaddr_in	SOCKADDR_IN; 		// sockaddr_in 구조체 타입명 설정
+	int							serverSockfd;    	// Socket fd
+	int							clientSockfd;    	// Socket fd
+	std::string					errMsg = ""; 		// 에러메시지
+	SOCKADDR_IN					serverAddr;  		// 서버의 소켓 주소 저장
+	SOCKADDR_IN					clientAddr;  		// 클라이언트의 소켓 주소 저장
+	int							portNum;    // 포트번호 저장
+
+	(void) clientSockfd;
+	(void) clientAddr;
+	try
+	{
+		if (ac >= 2)
+		{
+			try
+			{
+				//1. socket 생성
+				if ((serverSockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) //TODO - 왜 AF_INET가 아님?
+				{
+					errMsg = "Socket failed";
+					throw std::runtime_error(errMsg);
+				}
+				
+				//2. 서버의 소켓 주소 설정
+				std::memset(&serverAddr, 0, sizeof(serverAddr));
+				serverAddr.sin_family = AF_INET;
+				serverAddr.sin_addr.s_addr = htonl(INADDR_ANY); //TODO - s_addr?, INADDR_ANY는 netpractice의 default와 같은 의미?
+				portNum = std::atoi(av[1]);
+				serverAddr.sin_port = portNum;
+
+				//3. Bind
+				if (bind(serverSockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0)
+				{
+					throw std::runtime_error(strerror(errno));
+				}
+
+				//4. Listen
+				if (listen(serverSockfd, SOMAXCONN) < 0)
+				{
+					throw std::runtime_error(strerror(errno));
+				}
+			}
+			catch(const std::exception& e)
+			{
+				std::cerr << e.what() << std::endl;
+			}
+		}
+		else
+		{
+			errMsg = "Usage: " + std::string(av[0]) + "port";
+			throw std::runtime_error(errMsg);
+		}
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	return (0);
+}
