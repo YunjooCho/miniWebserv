c6e0cd5a699bc5d0c98092081e6e83a4157c615b
Yunjoo Cho <yunjcho@c1r7s6.42seoul.kr>
2023-11-02 21:44:43 +0900

    backup

---
+++ soonjeong/simple_server_main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+#include "ServerSocket.h"
+#include "SocketException.h"
+
+int main(int argc, char** argv) {
+  std::cout << "running....\n";
+
+  try {
+    // Create the socket
+    ServerSocket server(30000);
+
+    while (true) {
+      ServerSocket new_sock;
+      server.accept(new_sock);
+      std::cout << "accept" << std::endl;
+      try {
+        while (true) {
+          std::string data;
+          new_sock >> data; //Socket에 담긴 값을 data에 가져옴
+          new_sock << data;
+        }
+      } catch (SocketException&) {
+      }
+    }
+  } catch (SocketException& e) {
+    std::cout << "Exception was caught:" << e.description() << "\nExiting.\n";
+  }
+
+  return 0;
+}
