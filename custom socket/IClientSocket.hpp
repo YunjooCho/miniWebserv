0607b7b935241df60574e5f42d76c8f751dea7f4
Yunjoo Cho <yunjcho@student.42seoul.kr>
2023-12-01 17:04:23 +0900

    Make Classes

--- custom socket/IClientSocket.hpp
+++ custom socket/IClientSocket.hpp
@@ -1,15 +1,16 @@
 #ifndef ICLIENTSOCKET_HPP
-# define ICLIENTSOCKET_HPP
+#define ICLIENTSOCKET_HPP
 
-# include <iostream>
-# include <sys/socket.h>
+#include <sys/socket.h>
 
-class IClientSocket
-{
-	//socket
-	virtual void	socket(int domain, int type, int protocol, int kqFd) = 0;
-	virtual void	connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) = 0;
-	virtual void	close(int socketFd) = 0;
+#include <iostream>
+
+class IClientSocket {
+  // socket
+  virtual void socket(int domain, int type, int protocol, int kqFd) = 0;
+  virtual void connect(int sockfd, const struct sockaddr *addr,
+                       socklen_t addrlen) = 0;
+  virtual void close(int socketFd) = 0;
 };
 
 #endif
--- custom socket/Kqueue.hpp
+++ custom socket/Kqueue.hpp
@@ -1,21 +1,23 @@
 #ifndef CKQUEUE_HPP
-# define CKQUEUE_HPP
+#define CKQUEUE_HPP
 
-# include <sys/types.h>
-# include <sys/event.h>
-# include <vector>
+#include <sys/event.h>
+#include <sys/types.h>
 
-class Kqueue
-{
-	public:
-		void	kqueue(void);
-		void	changeEvents(uintptr_t ident, int16_t filter, uint16_t flags, uint32_t fflags, intptr_t data, void *udata);
-	private:
-		int	m_kqFd;
-		int	new_events;
-		std::vector<struct kevent>	m_changeList;
-		struct kevent				m_eventList[8];
-		struct kevent				*m_currEvent;
+#include <vector>
+
+class Kqueue {
+ public:
+  void kqueue(void);
+  void changeEvents(uintptr_t ident, int16_t filter, uint16_t flags,
+                    uint32_t fflags, intptr_t data, void *udata);
+
+ private:
+  int													m_kqFd;
+  int													new_events;
+  std::vector<struct kevent>	m_changeList;
+  struct kevent								m_eventList[8];
+  struct kevent								*m_currEvent;
 };
 
 #endif
